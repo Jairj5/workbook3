aeac2d7f818d5ca65fc4cbe31c25d76205514c69
Jair Jabriel <JJabriel@atl.yearup.org>
2022-10-03 11:46:48 -0400

    more value

---
+++ simple_functions.js
@@ -0,0 +1,18 @@
+function favoriteThings() {
+    console.log("food");
+    console.log("sports");
+    console.log("netflix");
+}
+
+function whyImHere() {
+    console.log("myself");
+    console.log("money");
+}
+
+function favoritePlace() {
+    console.log("favorite place miami because of weather ");
+}
+
+favoritePlace() 
+whyImHere()
+favoriteThings()
